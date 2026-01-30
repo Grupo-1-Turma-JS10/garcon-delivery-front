3c013dce7abd7efe8c277027c7a0e1330f90bdd7
Eduardo Pagel <pagel.edu@gmail.com>
2026-01-30 18:57:06 -0300

    feat: implementação inicial do layout da página de Cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Login } from './pages/Login/Login'
+import { Cadastro } from './pages/cadastro/Cadastro'
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Cadastro />} />
         </Routes>
       </BrowserRouter>
     </>
