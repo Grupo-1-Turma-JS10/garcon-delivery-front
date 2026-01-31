884c14d0cb8b2aa1bbba50d57a6d483c20356230
Joselaine Bechaire <git.bechaire@gmail.com>
2026-01-31 17:34:24 -0300

    add carrinho de compras

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Login } from './pages/Login/Login'
 import { Navbar } from './components/navbar/Navbar'
 import { ListaProdutos } from './pages/Login/ListaProdutos'
+import Cart from './components/carrinho/cart/Cart'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/produtos" element={<ListaProdutos />} />
+          <Route path="/carrinho" element={<Cart />} />
         </Routes>
       </BrowserRouter>
     </>
