06d3cd1debed573a44b9f830a5598c069a8d4057
Daniel Ribeiro <dann.ribeiroo@gmail.com>
2026-02-04 12:40:18 -0300

    fix: responsivity

--- src/pages/sobre/AboutUs.tsx
+++ src/pages/sobre/AboutUs.tsx
@@ -219,7 +219,7 @@ export const AboutUsPage: React.FC = () => {
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true }}
-          className="grid grid-cols-7 gap-2"
+          className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7 gap-2"
         >
           {teamMembers.map((member) => (
             <motion.div 
